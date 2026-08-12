eed668072523f71bf33e041c0aecdaf19fe55697
qwqgong-ui <248246420+qwqgong-ui@users.noreply.github.com>
2026-08-12 12:04:14 +0800

    build: bump version to 0.7.123

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "io.github.qwqgong.androidcyaml"
         minSdk = 36
         targetSdk = 37
-        versionCode = 235
-        versionName = "0.7.122"
+        versionCode = 236
+        versionName = "0.7.123"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
