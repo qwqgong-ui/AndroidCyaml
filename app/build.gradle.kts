a869e03176946baef657b4044d4ea919735e3fb8
qwqgong-ui <248246420+qwqgong-ui@users.noreply.github.com>
2026-08-30 09:08:04 +0800

    chore: bump version to 1.0.36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "io.github.qwqgong.androidcyaml"
         minSdk = 36
         targetSdk = 37
-        versionCode = 333
-        versionName = "1.0.35"
+        versionCode = 334
+        versionName = "1.0.36"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
